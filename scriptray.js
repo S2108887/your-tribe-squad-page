18286f5ca8677f95d2d0ea93e565b078d9161219
rayhana7 <rayhana.el.markouchi@hva.nl>
2026-09-13 18:08:24 +0200

    Visitekaartjes en JS
    Visitekaartjes toegevoegd in HTML, een beetje gewerkt in JS en css. en de zoekbalk laten werken.

---
+++ scriptray.js
@@ -0,0 +1,61 @@
+const searchInput = document.querySelector('input[type="search"]');
+const searchButton = document.querySelector("header button");
+
+searchInput.addEventListener("input", function () {
+    const zoekterm = searchInput.value.toLowerCase();
+
+    cards.forEach(function (card) {
+        const naam = card.querySelector("h2").textContent.toLowerCase();
+
+        if (naam.includes(zoekterm)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    });
+});
+
+const cards = document.querySelectorAll(".card-preview div");
+const arrows = document.querySelectorAll(".arrow");
+
+let currentCard = 0;
+
+function showCards() {
+    cards.forEach(function (card) {
+        card.style.display = "none";
+    });
+
+    cards[currentCard].style.display = "block";
+    cards[currentCard + 1].style.display = "block";
+    cards[currentCard + 2].style.display = "block";
+}
+
+showCards();
+
+arrows[0].addEventListener("click", function () {
+    currentCard -= 3;
+
+    if (currentCard < 0) {
+        currentCard = cards.length - 3;
+    }
+
+    showCards();
+});
+
+arrows[1].addEventListener("click", function () {
+    currentCard += 3;
+
+    if (currentCard >= cards.length) {
+        currentCard = 0;
+    }
+
+    showCards();
+});
+
+cards.forEach(function (card) {
+    card.addEventListener("click", function () {
+        const image = card.querySelector("img");
+
+        window.open(image.src, "_blank");
+    });
+});
